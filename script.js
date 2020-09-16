0b21f43abc7a3c5f5a3d82ab8a1b45d26d85c0f2
AlphaDown <stoda2005@gmail.com>
2020-09-16 21:34:02 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $(function() {
+      $("#draggableBox").draggable();
+      $("#draggableRect").draggable();
+      $("#draggableBox2").draggable();
+      $("#draggableRect2").draggable();
+      $("#draggableTri").draggable();
+      $("#draggableCircle").draggable();
+      $("#draggableCircle2").draggable();
+      $("#draggableCircle3").draggable();
+    })
+});
